e9430addf25b24dfdcc32a234160a719d5370f75
eyal462 <eyal462@gmail.com>
2023-03-02 09:38:02 +0200

    -test3

---
+++ SeLeNiUm/test_AOS_TestCases.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import unittest
+
+import unittest
+
+class TestStringMethods(unittest.TestCase):
